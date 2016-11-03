c9647cd2acb52065c71277beedcc777f35ce8258
Maurits van Rees <maurits@vanrees.org>
2016-11-03 15:01:59 +0100

    Reindented setup.py

--- setup.py
+++ setup.py
@@ -2,38 +2,39 @@ from setuptools import setup, find_packages
 
 version = '1.0b10'
 
-setup(name='collective.wtf',
-      version=version,
-      description="GenericSetup importer and exporter for workflow definitions that uses CSV instead of XML",
-      long_description=(open("README.rst").read() + "\n" +
-                        open("CHANGES.rst").read()),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='collective.wtf',
+    version=version,
+    description="GenericSetup importer and exporter for workflow definitions that uses CSV instead of XML",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='plone workflow genericsetup',
-      author='Martin Aspeli',
-      author_email='optilude@gmail.com',
-      url='http://plone.org',
-      license='LGPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.interface',
-          'zope.component',
-          'Products.CMFCore',
-          'Products.DCWorkflow',
-          'Products.GenericSetup',
-          'plone.memoize',
-
-      ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    ],
+    keywords='plone workflow genericsetup',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='http://plone.org',
+    license='LGPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'Products.CMFCore',
+        'Products.DCWorkflow',
+        'Products.GenericSetup',
+        'plone.memoize',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
